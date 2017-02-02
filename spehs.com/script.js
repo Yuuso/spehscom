67566ba8094fbc5a3143d7101af82b98b45aa455
Juuso Turunen <juusoturunen94@hotmail.com>
2017-02-02 23:26:06 +0200

    team drop menu

--- spehs.com/script.js
+++ spehs.com/script.js
@@ -1,17 +1,33 @@
 
 $(document).ready(function ()
 {
-    var numNavs = $("#navigationbar > a").length;
+    var numNavs = $("#navigationbar").find('.navigation').length;
     var navWidth = (900 - (numNavs - 1) * 8) / numNavs;
+    var navHeight = 60;
+
     $(".navigation").css("width", navWidth + "px");
-    $('.navigation').mouseenter(function ()
+    $(".navigation").css("height", navHeight + "px");
+    $(".subnavigationbox").css("width", navWidth + "px");
+    $(".subnavigationbox").css("height", navHeight + "px");
+
+    $('.navigation, .subnavigationbox').mouseenter(function ()
     {
         $(this).css('background-color', '#243333');
         $(this).css('color', '#26c97a');
+
+        var numSubNavs = $(this).children('.subnavigation').find('div').length;
+        $(this).children('.subnavigation').animate(
+        {
+            height: numSubNavs * navHeight + "px"
+        }, 90);
     });
-    $('.navigation').mouseleave(function ()
+    $('.navigation, .subnavigationbox').mouseleave(function ()
     {
         $(this).css('background-color', '#222222');
         $(this).css('color', '#185952');
+        $(this).children('.subnavigation').animate(
+        {
+            height: "0px"
+        }, 90);
     });
 });
